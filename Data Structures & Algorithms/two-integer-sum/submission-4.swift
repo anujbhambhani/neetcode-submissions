43e5965de80e202dbc5f84588f4f5850da0cd7b9
anujbhambhani <anuj.bhambhani@gmail.com>
2026-08-17 15:06:43 -0700

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        let sortedNums = nums.enumerated().map { ($0.element, $0.offset) }.sorted {$0.0 < $1.0}
+        var i = 0
+        var j = nums.count - 1
+        var res = [Int]()
+        while i < j {
+            if (sortedNums[i].0 + sortedNums[j].0 == target) {
+                res.append(sortedNums[i].1)
+                res.append(sortedNums[j].1)
+                return res.sorted()
+            } else if (sortedNums[i].0 + sortedNums[j].0 < target) {
+                i += 1
+            } else {
+                j -= 1
+            }
+        }
+        return res
+    }
+}
