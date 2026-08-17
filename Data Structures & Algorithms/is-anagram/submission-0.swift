5ef9c3df6203d677163b8b06d486c4171bd42ef0
anujbhambhani <anuj.bhambhani@gmail.com>
2026-08-17 10:45:12 -0700

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        if (s.count != t.count) { return false }
+        var sCount = [Character: Int]()
+        var tCount = [Character: Int]()
+        for ch in s {
+            sCount[ch, default:0] += 1
+        }
+        for ch in t {
+            tCount[ch, default:0] += 1
+        }
+        return sCount == tCount
+    }
+}
