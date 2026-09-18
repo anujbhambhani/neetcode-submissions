286e8acbbcaee4fd7ae6837f247ecc17b2e5c008
anujbhambhani <anuj.bhambhani@gmail.com>
2026-09-18 01:16:32 -0700

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.swift
@@ -0,0 +1,16 @@
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        var l = 0, r = nums.count - 1
+        while (l <= r) {
+            var m = l + (r - l) / 2
+            if (nums[m] == target) {
+                return m
+            } else if (nums[m] < target) {
+                l = m + 1
+            } else {
+                r = m - 1
+            }
+        }
+        return -1
+    }
+}
